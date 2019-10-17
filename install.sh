2a088ac290898750986582151370a2a1d345d1e9
Jin Whan Bae <4ib@ornl.gov>
2019-10-17 11:33:25 -0400

    tar it up

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-pyinstaller --add-data "./src/*:./src/" --onefile gui.spec
-tar -czvf dist ./dist/gui
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $SCRIPTPATH
+pyinstaller --add-data "./src/*:./src/" --onefile $SCRIPTPATH/gui.spec
+tar -czvf $SCRIPTPATH/dist/dist.tar.gz $SCRIPTPATH/dist/gui
